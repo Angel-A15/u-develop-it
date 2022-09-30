bc791db7046a86e1167ddc30830ee24d44c9bc12
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-09-30 08:49:14 -0500

    on create query for read operation

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//Will connect MySQL database
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +9,24 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// // Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'angel',
+      // Your MySQL password
+      password: 'Scyt54ma!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Query the database to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (not found)
 app.use((req, res) => {
