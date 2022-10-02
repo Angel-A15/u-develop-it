4856b03c616c0d7966d0670f5e3aec2b6605c605
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-02 00:24:47 -0500

    updated the get routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+-- drop/delete the tables every time you run the schema.sql file, ensuring that you start with a clean slate
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
