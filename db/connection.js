1807b1cf02d0d740e8f774fc23488aad3cdd1740
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-05 07:56:12 -0500

    edited file

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // Your MySQL username,
-      user: 'root',
+      user: '',
       // Your MySQL password
       password: '',
       database: 'election'
